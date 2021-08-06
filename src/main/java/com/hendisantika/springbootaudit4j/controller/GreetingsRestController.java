55e6a12d6c30635b32dcd3e8b4dc2c3507d34d51
Hendi Santika <hendisantika@gmail.com>
2021-08-07 06:13:24 +0700

    #13 - Bug Fix in GreetingsRestController

--- src/main/java/com/hendisantika/springbootaudit4j/controller/GreetingsRestController.java
+++ src/main/java/com/hendisantika/springbootaudit4j/controller/GreetingsRestController.java
@@ -2,6 +2,7 @@ package com.hendisantika.springbootaudit4j.controller;
 
 import com.hendisantika.springbootaudit4j.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
