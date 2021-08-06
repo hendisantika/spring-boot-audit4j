2d08d2cb9cdb167c91e4cd93aed19fb39cb04461
Hendi Santika <hendisantika@gmail.com>
2021-08-07 05:55:23 +0700

    #7 - Add springAudit4jConfig bean in AuditConfiguration

--- src/main/java/com/hendisantika/springbootaudit4j/config/AuditConfiguration.java
+++ src/main/java/com/hendisantika/springbootaudit4j/config/AuditConfiguration.java
@@ -1,15 +1,24 @@
 package com.hendisantika.springbootaudit4j.config;
 
 import org.audit4j.core.MetaData;
+import org.audit4j.core.handler.ConsoleAuditHandler;
+import org.audit4j.core.handler.Handler;
 import org.audit4j.core.handler.file.FileAuditHandler;
 import org.audit4j.core.layout.Layout;
 import org.audit4j.core.layout.SimpleLayout;
 import org.audit4j.handler.db.DatabaseAuditHandler;
+import org.audit4j.integration.spring.AuditAspect;
+import org.audit4j.integration.spring.SpringAudit4jConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.env.Environment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -52,5 +61,32 @@ public class AuditConfiguration {
     public FileAuditHandler fileAuditHandler() {
         FileAuditHandler fileAuditHandler = new FileAuditHandler();
         return fileAuditHandler;
+    }
+
+    @Bean
+    public ConsoleAuditHandler consoleAuditHandler() {
+        return new ConsoleAuditHandler();
+    }
+
+    @Bean
+    public SpringAudit4jConfig springAudit4jConfig() {
+        SpringAudit4jConfig audit4jConfig = new SpringAudit4jConfig();
+        Map<String, String> props = new HashMap<>();
+        props.put("log.file.location", ".");
+        List<Handler> handlers = new ArrayList<>();
+        handlers.add(consoleAuditHandler());
+        handlers.add(fileAuditHandler());
+        handlers.add(databaseHandler());
+        audit4jConfig.setHandlers(handlers);
+        audit4jConfig.setLayout(layout());
+        audit4jConfig.setMetaData(metaData());
+        audit4jConfig.setProperties(props);
+        return audit4jConfig;
+
+    }
+
+    @Bean
+    public AuditAspect auditAspect() {
+        return new AuditAspect();
     }
 }
