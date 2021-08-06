31eff42665c20b4b6bc19b32eddc8b79c25dd68d
Hendi Santika <hendisantika@gmail.com>
2021-08-07 05:59:35 +0700

    #12 - Add welcome in GreetingsRestController

--- src/main/java/com/hendisantika/springbootaudit4j/controller/GreetingsRestController.java
+++ src/main/java/com/hendisantika/springbootaudit4j/controller/GreetingsRestController.java
@@ -2,6 +2,9 @@ package com.hendisantika.springbootaudit4j.controller;
 
 import com.hendisantika.springbootaudit4j.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,4 +21,14 @@ public class GreetingsRestController {
 
     @Autowired
     HelloService helloService;
+
+    @GetMapping(value = {"/", "/welcome"}, produces = "application/json")
+    public String welcome() {
+        String name = "anonymous";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            name = ((UserDetails) authentication.getPrincipal()).getUsername();
+        }
+        return helloService.hello(name);
+    }
 }
